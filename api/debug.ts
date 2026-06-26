b80a7714d83e9677e54838a3538a7f64fc9f56b8
Veritas ERP Fix <veritas-erp@local>
2026-06-26 02:35:07 +0000

    Test _password import

--- api/debug.ts
+++ api/debug.ts
@@ -1,5 +1,4 @@
-// Test: can we even import _auth.ts at all?
-import { getJwtSecret } from "./_auth";
+import { verifyPassword, safeEqual } from "./_password";
 
 export default async function handler(req: any, res: any) {
   try {
